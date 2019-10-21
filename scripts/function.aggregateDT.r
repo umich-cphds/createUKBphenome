a7f3252b270f7da740309d7e42972e5d3840d459
Lars <fritsche.lars@gmail.com>
2019-10-21 13:06:04 -0400

    Cleanup

--- scripts/function.aggregateDT.r
+++
@@ -1,7 +0,0 @@
-aggregateDT <- function(dataIn,field,fieldname,FUN,id="id"){
-	out <- data.table(aggregate(x = dataIn[[field]],
-                               by = list('id'=dataIn[[id]]),
-                               FUN = FUN,na.rm=TRUE, na.action=NULL))
-	setnames(out,c("id","x"),c(id,fieldname))
-	return(out)
-}
--- scripts/function.mergeMultiple.r
+++
@@ -1,7 +0,0 @@
-require("data.table")
-mymerge <- function(x,y) merge(x,y,by="id",all=TRUE)
-mergeMultiple <- function(dtlist) {
-	if(!is.list(dtlist)) stop("Not a list of data.tables / data.frames")
-	Reduce(mymerge,dtlist)
-}
-
